6bc29f27b9facfb9c38ca615d9f59f383269d289
thisismrsanjay <thisismrsanjay@gmail.com>
2018-12-01 07:40:25 +0530

    Updates

---
+++ precache-manifest.9f2284ea7890f41c20befe8ab1a912ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "95cfb1d09c9bd4491a2d",
+    "url": "/crud-router-cj/static/js/runtime~main.95cfb1d0.js"
+  },
+  {
+    "revision": "f0caab80d6269e29c356",
+    "url": "/crud-router-cj/static/js/main.f0caab80.chunk.js"
+  },
+  {
+    "revision": "0757237da337cfee0b0d",
+    "url": "/crud-router-cj/static/js/1.0757237d.chunk.js"
+  },
+  {
+    "revision": "f0caab80d6269e29c356",
+    "url": "/crud-router-cj/static/css/main.6bbab2f7.chunk.css"
+  },
+  {
+    "revision": "0757237da337cfee0b0d",
+    "url": "/crud-router-cj/static/css/1.36f08b4a.chunk.css"
+  },
+  {
+    "revision": "235e8555e8d6a9ac171d1c7db570876d",
+    "url": "/crud-router-cj/index.html"
+  }
+];
